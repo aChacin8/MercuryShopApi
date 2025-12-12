e20658797cfae2e227a86734e672a81cffe8dda9
aChacin8 <alechacin08@gmail.com>
2025-12-12 12:36:43 -0600

    feat: Primeros pasos en el servicio de Infraestructura para Hashear la contraseña, usando la interfaz de la contraseña

---
+++ MercuryShop.Infrastructure/Services/PasswordHasher.cs
@@ -0,0 +1,10 @@
+using BCrypt.Net;
+using MercuryShop.Application.Interfaces;
+
+namespace MercuryShop.Infrastructure.Services
+{
+    public class PasswordHasher : IPasswordHasher
+    {
+        
+    }
+}
