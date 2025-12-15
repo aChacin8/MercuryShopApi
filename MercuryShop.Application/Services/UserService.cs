133f3c4a0a636dbcb4d68429b14eba957fdb1ac9
aChacin8 <alechacin08@gmail.com>
2025-12-15 16:55:48 -0600

    feat: Escribiendo parte de UserService, agregando las interfaces que se usaran para el servicio

--- MercuryShop.Application/Services/UserService.cs
+++ MercuryShop.Application/Services/UserService.cs
@@ -1,11 +1,25 @@
 using MercuryShop.Application.DTOs;
+using MercuryShop.Application.Interfaces;
 using MercuryShop.Domain.Entities;
-
 
 namespace MercuryShop.Application.Services
 {
     public class UserService
     {
-        
+        private readonly IUserRepository _userRepository; 
+        private readonly IPasswordHasher _passwordHash;
+        private readonly IJwtService _jwtService;
+
+        public UserService(IUserRepository userRepository, IPasswordHasher passwordHash, IJwtService jwtService)
+        {
+            _userRepository = userRepository;
+            _passwordHash = passwordHash;
+            _jwtService = jwtService; 
+        }
+
+        // public async Task<string> RegisterUser(RegisterUserDto registerUserDto)
+        // {
+            
+        // }
     }
 }
