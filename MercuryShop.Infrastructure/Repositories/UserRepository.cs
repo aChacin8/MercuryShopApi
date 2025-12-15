605a23e38f75e1077cc377420308a80bc4633374
aChacin8 <alechacin08@gmail.com>
2025-12-15 16:57:26 -0600

    feat: Creamos User Repository inicial. (lo comentamos para evitar errores)

---
+++ MercuryShop.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,19 @@
+using MercuryShop.Domain.Entities;
+using MercuryShop.Application.DTOs;
+using MercuryShop.Application.Interfaces;
+
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace MercuryShop.Infrastructure.Repositories
+{
+    // public class UserRepository : IUserRepository
+    // {
+    //     private readonly MercuryShopDbContext _context;
+
+    //     public UserRepository(MercuryShopDbContext context)
+    //     {
+    //         _context = context;
+    //     }
+    // }
+}
