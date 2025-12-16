c3dab79ded85ed8f3d9100f63cf679b8f4dd0527
mrMcAko <alechacin08@gmail.com>
2025-12-15 20:33:48 -0600

    feat: Creamos el Program.cs que generará las inyecciones de dependencia y lectura de campos en appsettings, además de la autenticación y autorización asi como la validacion de estas

--- MercuryShop.Api/Program.cs
+++ MercuryShop.Api/Program.cs
@@ -1,41 +1,70 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+using MercuryShop.Application.Interfaces;
+using MercuryShop.Infrastructure.Data;
+using MercuryShop.Infrastructure.Repositories;
+using MercuryShop.Infrastructure.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer(); //Endpoint explorer para Swagger
+builder.Services.AddSwaggerGen(); //Visualizador Swagger
+
+builder.Services.AddDbContext<MercuryShopDb>(options =>
+{
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection"); //Lee los datos de la conexión, definidos en appsettings.json
+
+    options.UseMySql(
+        connectionString,
+        ServerVersion.AutoDetect(connectionString) //Detecta la version de base de datos que se esta usando 
+    );
+}); // Conexion a MySQL
+
+builder.Services.AddScoped<IUserRepository, UserRepository>(); // () indica que es metodo, <> Son los tipos, cuando se pida IU va a entregar la instancia UserRep...
+builder.Services.AddScoped<IPasswordHasher, PasswordHasher>();
+builder.Services.AddScoped<IJwtService, JwtService>();
+
+
+var jwtKey = builder.Configuration["Jwt:Key"];
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(
+            Encoding.UTF8.GetBytes(jwtKey!)
+        )
+    };
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.UseAuthentication();
+app.UseAuthorization();
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
