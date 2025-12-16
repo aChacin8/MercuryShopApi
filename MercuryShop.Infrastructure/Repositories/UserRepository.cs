ff488855053c14d49bba5583ab18ab9f600f4637
mrMcAko <alechacin08@gmail.com>
2025-12-16 14:50:45 -0600

    feat: Terminamos de crear el UserRepository, consumiendo la instancia de la db, guardando los cambios en la BD y creando metodo para GetEmail

--- MercuryShop.Infrastructure/Repositories/UserRepository.cs
+++ MercuryShop.Infrastructure/Repositories/UserRepository.cs
@@ -1,19 +1,31 @@
-using MercuryShop.Domain.Entities;
-using MercuryShop.Application.DTOs;
-using MercuryShop.Application.Interfaces;
-
 using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
+
+using MercuryShop.Domain.Entities;
+using MercuryShop.Infrastructure.Data;
+using MercuryShop.Application.Interfaces;
+
 
 namespace MercuryShop.Infrastructure.Repositories
 {
     public class UserRepository : IUserRepository
     {
-        private readonly MercuryShopDbContext _context;
+        private readonly MercuryShopDb _context;
 
-        public UserRepository(MercuryShopDbContext context)
+        public UserRepository(MercuryShopDb context)
         {
             _context = context;
+        }
+
+        public async Task AddAsync(User user)
+        {
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<User?> GetEmail (string email)
+        {
+            return await _context.Users.SingleOrDefaultAsync(u => u.Email == email); //SingleOrDefault, para mantener unicidad, es decir email unico.
         }
     }
 }
