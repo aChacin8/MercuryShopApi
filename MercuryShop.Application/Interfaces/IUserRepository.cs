fedd643e625c50e87476c9adf63109af63e3dbd0
mrMcAko <alechacin08@gmail.com>
2025-12-16 14:49:35 -0600

    feat: Se agregó el metodo para obtener el email

--- MercuryShop.Application/Interfaces/IUserRepository.cs
+++ MercuryShop.Application/Interfaces/IUserRepository.cs
@@ -1,10 +1,13 @@
-using MercuryShop.Domain.Entities;
 using System.Threading.Tasks;
+
+using MercuryShop.Domain.Entities;
+
 
 namespace MercuryShop.Application.Interfaces
 {
     public interface IUserRepository
     {
         Task AddAsync(User user);
+        Task <User?> GetEmail (string email);
     }
 }
