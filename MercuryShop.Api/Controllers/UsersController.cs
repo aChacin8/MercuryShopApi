8531d3a50799a5105066d1a85196b075361c66b3
mrMcAko <alechacin08@gmail.com>
2025-12-22 20:42:22 -0600

    feat: Terminamos la creacion del controlador para crear usuarios

--- MercuryShop.Api/Controllers/UsersController.cs
+++ MercuryShop.Api/Controllers/UsersController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 
 using MercuryShop.Application.Interfaces;
 using MercuryShop.Application.DTOs;
+using MercuryShop.Domain.Entities;
 
 namespace MercuryShop.Api.Controllers
 {
@@ -9,6 +10,32 @@ namespace MercuryShop.Api.Controllers
     [Route("api/[controller]")]
     public class UsersControllers : ControllerBase
     {
-        private readonly IUserRepository _userRepository;
+        private readonly IUserService _userService;
+
+        public UsersControllers(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost("register")]
+        public async Task <IActionResult> Register (RegisterUserDto registerUserDto)
+        {
+            await _userService.RegisterUser(registerUserDto);
+            return Ok(new
+            {
+                message = "User registered succesfully"
+            });
+        }
+
+        [HttpPost("login")]
+        public async Task <IActionResult> Login (LoginUserDto loginUserDto)
+        {
+            var token = await _userService.LoginUser(loginUserDto);
+            return Ok(new
+            {
+                token,
+                message = "User login succesfully"
+            });
+        }
     }
 }
