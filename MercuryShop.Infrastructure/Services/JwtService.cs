ef45fb71e1feae50897621c4ffcf4b4c873636c2
mrMcAko <alechacin08@gmail.com>
2025-12-22 22:29:57 -0600

    feat: Agregamos y mejoramos la configuracion para que se lea expirationMinutes para JWT

--- MercuryShop.Infrastructure/Services/JwtService.cs
+++ MercuryShop.Infrastructure/Services/JwtService.cs
@@ -14,20 +14,27 @@ namespace MercuryShop.Infrastructure.Services
         private readonly string _secretKey;
         private readonly string _issuer;
         private readonly string _audience;
-        private readonly int _expiryMinutes;
+        private readonly int _expirationMinutes;
 
-        public JwtService (IConfiguration configuration)
+        public JwtService(IConfiguration configuration)
         {
             _secretKey = configuration["Jwt:SecretKey"]!;
             _issuer = configuration["Jwt:Issuer"]!;
             _audience = configuration["Jwt:Audience"]!;
-            _expiryMinutes = int.Parse(configuration["Jwt:ExpiryMinutes"]!);
+            var expirationString = configuration["Jwt:ExpirationMinutes"];
+
+            if (!int.TryParse(expirationString, out var expirationMinutes))
+            {
+                throw new Exception("Jwt:ExpirationMinutes is missing or invalid");
+            }
+
+            _expirationMinutes = expirationMinutes;
         }
 
-        public string GenerateToken (UserClaims userClaims)
+        public string GenerateToken(UserClaims userClaims)
         {
             var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_secretKey));
-            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256 );
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
 
             var claims = new[]
             {
@@ -40,8 +47,8 @@ namespace MercuryShop.Infrastructure.Services
                 issuer: _issuer,
                 audience: _audience,
                 claims: claims,
-                expires: DateTime.UtcNow.AddMinutes(_expiryMinutes),
-                signingCredentials: credentials 
+                expires: DateTime.UtcNow.AddMinutes(_expirationMinutes),
+                signingCredentials: credentials
             );
             return new JwtSecurityTokenHandler().WriteToken(token);
         }
