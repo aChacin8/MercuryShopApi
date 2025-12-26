5cc2ae3b02da189ecf17a00330bef7e0bd929013
mrMcAko <alechacin08@gmail.com>
2025-12-26 14:33:06 -0600

    feat: Agregamos el UpdateAsync y el GetByIdAsync al UserRepository

--- MercuryShop.Infrastructure/Repositories/UserRepository.cs
+++ MercuryShop.Infrastructure/Repositories/UserRepository.cs
@@ -23,9 +23,20 @@ namespace MercuryShop.Infrastructure.Repositories.Auth
             await _context.SaveChangesAsync();
         }
 
+        public async Task UpdateAsync (User user)
+        {
+            _context.Users.Update(user);
+            await _context.SaveChangesAsync();
+        }
+
         public async Task<User?> GetByEmailAsync (string email)
         {
             return await _context.Users.SingleOrDefaultAsync(u => u.Email == email); //SingleOrDefault, para mantener unicidad, es decir email unico.
+        }
+
+        public async Task<User?> GetByIdAsync (int Id)
+        {
+            return await _context.Users.SingleOrDefaultAsync(u => u.Id == Id);
         }
     }
 }
