5433c6a3ee1b4d7b8dbe601632ab418183e9b962
mrMcAko <alechacin08@gmail.com>
2025-12-26 14:26:33 -0600

    feat: Creamos el Dto para actualizar el usuario, teniendo unicamente como requisitos obligatorios el id, y el currentPassword

--- MercuryShop.Application/DTOs/Auth/UpdateUserDto.cs
+++ MercuryShop.Application/DTOs/Auth/UpdateUserDto.cs
@@ -1,4 +1,16 @@
 namespace MercuryShop.Application.DTOs.Auth
 {
-    
+    public class UpdateUserDto
+    {
+        public required int Id {get; set;}
+        public required string CurrentPassword {get; set;}
+
+        public string? Email {get; set;}
+
+        public string? FirstName { get; set;}
+        public  string? LastName { get; set; }
+        public  string? NewPassword { get; set; }
+        public string?  PhoneNumber { get; set; }
+        public string? Address { get; set; }
+    }
 }
