86e5b8cc80f6bf4d44a9f63f31cdd2019f2cdf35
mrMcAko <alechacin08@gmail.com>
2025-12-26 14:27:33 -0600

    feat: Creamos y modificamos la Interface del usuario, para actualizar el usuario y obtener por Id

--- MercuryShop.Application/Interfaces/Auth/IUserRepository.cs
+++ MercuryShop.Application/Interfaces/Auth/IUserRepository.cs
@@ -8,6 +8,8 @@ namespace MercuryShop.Application.Interfaces.Auth
     public interface IUserRepository
     {
         Task AddAsync(User user);
+        Task UpdateAsync (User user);
         Task <User?> GetByEmailAsync (string email);
+        Task <User?> GetByIdAsync (int Id);
     }
 }
